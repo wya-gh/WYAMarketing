fa50f3e2e7e556bc6493957b569c44ff731df67b
Jamie Legg <legg.jamie@gmail.com>
2021-10-19 22:50:36 +0100

    some content

--- components/landingContent.tsx
+++ components/landingContent.tsx
@@ -211,10 +211,12 @@ export default function LandingContent() {
                 <div className="absolute inset-0 bg-gradient-to-r from-purple-300 to-indigo-700 mix-blend-multiply" />
               </div>
               <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
+                <div className="bg-gray-100 bg-blur dark:bg-gray-900 dark:bg-opacity-60 backdrop-filter bg-clip-padding backdrop-blur-xl bg-opacity-60 w-max mx-auto p-4">
                 <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
                   <span className="block text-white">Connect Your Spaces</span>
                   <span className="block text-indigo-200">Reduce Your Risks</span>
                 </h1>
+                
                 <p className="mt-6 max-w-lg mx-auto text-center text-xl text-indigo-200 sm:max-w-3xl">
                   Offline Bluetooth connectivity for your Events and Venues
                 </p>
@@ -233,6 +235,7 @@ export default function LandingContent() {
                       Live demo
                     </a>
                   </div>
+                </div>
                 </div>
               </div>
             </div>
